6c3e49ae6a42634ee6fc76582b3a91c585ad3ba2
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2009-12-16 18:22:59 +0900

    update Usage

--- pxemultiboot-helper.rb
+++ pxemultiboot-helper.rb
@@ -3,11 +3,13 @@
 =begin
 = PXE Multi Boot Helper
 == Requirements
-* Ruby 1.8.x
+* Ruby 1.8.x or later
 * wget
 
 == Usage
-* ruby pxemultiboot-helper.rb
+* ruby pxemultiboot-helper.rb --help
+* ruby pxemultiboot-helper.rb --debian lenny,etch,etchnhalf,squeeze,sid --ubuntu karmic,hardy,jaunty,intrepid,dapper,lucid --fedora 12,11,10,9 --centos 5.4,5.3,4.8,4.7 --vine 5.0,4.2 --mbm 0.39 --plop-boot-manager 5.0.4 --freedos-balder10 --gag 4.10 --ping 3.00.03
+
 
 == License
 The MIT License
